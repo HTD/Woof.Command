5abdeebade92bbff999c1555b2bf8abd0ad5f202
Adam Łyskawa <adam@codedog.pl>
2019-07-25 11:57:30 +0200

    Add project files.

---
+++ Woof.Command.Test/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Woof.Command.Test {
+
+    class Program {
+
+        /**
+         * Type "man" to see internal commands.
+         * Use Tab key a lot to see a list of possible completions.
+         */
+
+        static void Main(string[] args) {
+            var cli = new CommandShell(); // creates a new default commands hell, a little CMD on steroids.
+            cli.Start(); // starts the shell session.
+        }
+
+    }
+
+}
